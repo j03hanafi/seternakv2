006273498d084e6ad57bb7081afbee43d77217ad
j03hanafi <namoapak14@gmail.com>
2021-11-27 23:31:30 +0700

    update config

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$host = "localhost";
-$dbname = "seternak2";
-$user = "postgres";
-$port = "5433";
-$password = "12345678";
+$host = "ec2-18-206-108-36.compute-1.amazonaws.com";
+$dbname = "dvaq6sqisd3nt";
+$user = "qnfsbntdmiqfum";
+$port = "5432";
+$password = "ec57e496747e683bd6f73cc2032c4d929c207005e3c8780ccbf6057d34a58901";
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);  
 
